453e1fe8e434806ce753b6cda723d3e0d263ef8f
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-06-08 17:32:56 +0200

    add works.service.ts

---
+++ src/app/service/works.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorksService {
+
+  constructor() { }
+
+  //https://github.com/fortzsu/hand-history-replayer-app - csak Java
+  //https://github.com/fortzsu/hand-history-replayer-spring - java+angular works
+  //https://github.com/fortzsu/theater_reticket - theater-reticket
+  //https://github.com/fortzsu/angular-cv-bozso-fort
+}
