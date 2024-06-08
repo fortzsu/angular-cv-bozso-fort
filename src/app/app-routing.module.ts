7727510f9571a772caff840cba1bd73328174a01
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-06-08 20:40:10 +0200

    add skills to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {WorksComponent} from "./infos/works/works.component";
 import {ExperienceComponent} from "./infos/experience/experience.component";
 import {EducationComponent} from "./infos/education/education.component";
+import {SkillsComponent} from "./infos/skills/skills.component";
 
 const routes: Routes = [
   {path: '', component: WorksComponent},
   {path: 'works', component: WorksComponent},
   {path: 'experience', component: ExperienceComponent},
-  {path: 'education', component: EducationComponent}
+  {path: 'education', component: EducationComponent},
+  {path: 'skills', component: SkillsComponent}
 ];
 
 @NgModule({
