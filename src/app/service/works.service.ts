810d0ae9e0c979d55a64d224a7991205d8f3622b
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-06-09 09:27:17 +0200

    Add description to first work

--- src/app/service/works.service.ts
+++ src/app/service/works.service.ts
@@ -9,7 +9,8 @@ export class WorksService {
   constructor() { }
 
   works: WorkInfoModel[] = [
-    new WorkInfoModel('Theatre Reticket', 'description',
+    new WorkInfoModel('Theatre Reticket', 'Backend application for (event)ticket handling, where the users - admins and visitors - could reserve, buy and resell tickets. The admins have access to extended functions like reporting and managing visitor user accounts, creating events (e.g. plays and performances).\n' +
+      'This is a project for practicing Java/Spring Boot/SQL and expand my horizon with other technologies (OAuth 2.0 and Google API, Google Sheets API, JPA Criteria Queries).\n',
       'https://github.com/fortzsu/theater_reticket'),
     new WorkInfoModel('Hand history replayer - Angular & Java', 'desciption',
       'https://github.com/fortzsu/hand-history-replayer-spring'),
