7c89845ac42efa02c0d805be8503fb3c8b5dd4dd
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-06-10 15:52:33 +0200

    add Firebase

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+import firebase from "firebase/compat/app";
+import initializeApp = firebase.initializeApp;
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2rUy9QSSYxrCiw6j_jMHCcBiwBG-anLk",
+  authDomain: "cv-bozso-fort.firebaseapp.com",
+  projectId: "cv-bozso-fort",
+  storageBucket: "cv-bozso-fort.appspot.com",
+  messagingSenderId: "1085693725249",
+  appId: "1:1085693725249:web:31d62869124857aaaf54c0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
