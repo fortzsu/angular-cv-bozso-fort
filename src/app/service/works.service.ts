410acb39cc3553dd016900c64ac72dbed2478146
fortzsu <67285950+fortzsu@users.noreply.github.com>
2024-07-03 16:11:43 +0200

    refact text

--- src/app/service/works.service.ts
+++ src/app/service/works.service.ts
@@ -12,22 +12,22 @@ export class WorksService {
     new WorkInfoModel(
       'Theatre Reticket',
       ['Java', 'Spring Boot', 'REST API', 'SQL', 'OAuth 2.0', 'Google (Sheets) API', 'JPA Criteria Queries'],
-      'Event ticket (re)handling application with reporting.',
+      'Event ticket (re)handling application with reporting',
       'https://github.com/fortzsu/theater_reticket'),
     new WorkInfoModel(
       'Hand history replayer',
       ['Java', 'Angular', 'REST API', 'Spring Boot'],
-      'desciption',
+      'Full-Stack App for parsing Poker Hands and display values',
       'https://github.com/fortzsu/hand-history-replayer-spring'),
     new WorkInfoModel(
       'Hand history replayer',
       ['Java', 'HTML'],
-      'desciption',
+      'Backend App for parsing Poker Hands from .txt to Java objects',
       'https://github.com/fortzsu/hand-history-replayer-app'),
     new WorkInfoModel(
-      'CV site',
+      'This CV site',
       [],
-      'description',
+      'Angular - Firebase',
     'https://github.com/fortzsu/angular-cv-bozso-fort')
   ]
 
